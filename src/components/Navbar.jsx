58549310eec5e6af0e89f1f507bd73a819a79484
chefbrams <oldcitycatering@gmail.com>
2022-02-16 14:24:34 -0500

    removed text decoration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ function Navbar() {
         <div className="row">
             <div className="col home">
                 <NavLink to="/" className={({isActive}) =>
-                    isActive ? "link-warning" : 'link-light'
+                    isActive ? "link-warning home" : 'link-light home'
                    
                 }>Home
 
                 </NavLink>
 
             </div>
-            <div className="col">
-            <NavLink to="/contact" className={({isActive}) => isActive ? "link-warning" : "link-light"
+            <div className="col contact">
+            <NavLink to="/contact" className={({isActive}) => isActive ? "link-warning contact" : "link-light contact"
                     
                    
                 }>Contact
